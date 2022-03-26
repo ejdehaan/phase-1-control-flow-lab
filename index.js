8030cec399500363dcac6dbf920615260b3ff823
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-26 07:34:15 -0400

    Could not pass all tests

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber > 2500){ 
+    result = 'No can do.' 
+      } 
+  else if (someNumber > 2000){ 
+    result = 'I will gladly take your thirty bucks.'
+    } 
+  else if (someNumber <= 400){
+    result = 'This one is on me!'
+    }
+  return result
+  }
+
+function ternaryCheckCity(NYC) {
+  return (ternaryCheckCity ? 'Ok, sounds good.' : 'No go.');
+}
+ternaryCheckCity(Pittsburgh);
+
+
+const switchOnCharmFromTip = 'generous'
+let tip
+switch (switchOnCharmFromTip) {
+    case 'generous':
+      tip = 'Thank you so much.'
+      break
+    case 'not as generous':
+      tip = 'Thank you.'
+      break
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
